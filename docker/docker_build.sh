0ac155bfe30d4a28af8d55117246c1884befdcec
robotarium-workstation <you@example.com>
2019-06-13 15:55:41 -0400

    Docker changes

--- docker/docker_build.sh
+++ docker/docker_build.sh
@@ -1,1 +1,8 @@
-docker build --tag robotarium:matlab_verifier .
+
+
+if [ "$1" == "" ]; then
+	echo "Argument 1 must be name of MATLAB container"
+	exit
+fi
+
+sudo docker build --build-arg MATLAB_CONTAINER="$1" --tag robotarium:matlab_verifier .
--- docker/docker_run.sh
+++ docker/docker_run.sh
@@ -1,1 +1,14 @@
+# 1: Robotarium host IP
 
+
+if [ "$1" == "" ]; then
+	echo "Argument 1 must be Robotarium verifier config path"
+	exit
+fi
+
+sudo docker run -ti --rm --name verifier \
+        --mac-address="98:90:96:cf:c7:c8" \
+        --env="DISPLAY" \
+	--env "ROBOTARIUM_VERIFIER_CONFIG=$1" \
+        -v /tmp/.X11-unix/:/tmp/.X11-unix/:rw \
+	robotarium:matlab_verifier
